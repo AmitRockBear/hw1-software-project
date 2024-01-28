83f8d8f36ed9707619fb47ad8dc3395fba402ba1
ExcaliBear <amitrock6789@gmail.com>
2024-01-28 17:53:58 +0200

    Replaced code with functions

--- main.c
+++ main.c
@@ -5,20 +5,7 @@
 #define eps 0.0001
 #define MAX_LINE_LENGTH 1024
 
-int countCommas(const char *str) {
-    int count = 0;
-
-    while (*str) {
-        if (*str == ',') {
-            count++;
-        }
-        str++;
-    }
-
-    return count;
-}
-
-double distance(double* vec1, double* vec2, int size) {
+double calculate_distance(double* vec1, double* vec2, int size) {
   double sum = 0;
   
   for (int i=0; i<size; i++) {
@@ -27,8 +14,133 @@ double distance(double* vec1, double* vec2, int size) {
 
   return sqrt(sum);
 }
+double** input_file_to_matrix(FILE* fp, int rows, int columns) {
+  double** vectors = (double**)calloc(rows, sizeof(double *));
+  if (vectors == NULL) {
+          fprintf(stderr, "Memory allocation failed\n");
+          exit(1);
+  }
 
+  int line_count = 0;
+  char line[MAX_LINE_LENGTH];
 
+  while (fgets(line, sizeof(line), fp) != NULL && line_count < rows) {
+      line[strcspn(line, "\n")] = '\0';
+
+      vectors[line_count] = calloc(columns, sizeof(double));
+
+      if (vectors[line_count] == NULL) {
+          fprintf(stderr, "Memory allocation failed\n");
+          exit(1);
+      }
+
+      char *token = strtok(line, ",");
+
+      int tokensCounter = 0;        
+      while (token != NULL) {
+          vectors[line_count][tokensCounter] = strtod(token, NULL);
+          
+          token = strtok(NULL, ",");
+          tokensCounter++;
+      }
+      line_count++;
+  }
+
+  return vectors;
+}
+double** deep_copy_matrix(double** copied_matrix, int rows, int columns) {
+  double** new_matrix = calloc((size_t)rows, sizeof(double *));
+  if (new_matrix == NULL) {
+      fprintf(stderr, "Memory allocation failed\n");
+      exit(1);
+  }
+
+  for (int i=0; i<rows; i++) {
+      new_matrix[i] = calloc(columns, sizeof(double));
+      for (int j=0; j<columns; j++) {
+        new_matrix[i][j] = copied_matrix[i][j];
+      }
+  }
+
+  return new_matrix;
+}
+
+int find_closest_centroid_to_vector_index(double* vector, int vector_size, double** centroids, int centroids_num) {
+  double min_distance = calculate_distance(vector, centroids[0], vector_size);
+  int min_j = 0;
+
+  for (int j=0; j<centroids_num; j++) {
+    double distance_from_centroid = calculate_distance(vector, centroids[j], vector_size);
+    if (min_distance > distance_from_centroid) {
+      min_distance = distance_from_centroid;
+      min_j = j;
+    }
+  }
+
+  return min_j;
+}
+
+double* create_new_centroid(double* centroid_sum, int centroid_counter, int centroid_size) {
+  double* new_centroid = calloc(centroid_size, sizeof(double));
+
+  for (int p=0; p<centroid_size; p++) {
+    new_centroid[p] = centroid_sum[p] / centroid_counter;
+  }
+
+  return new_centroid;
+}
+
+void calculate_centroids_convergence(double** centroids, double** vectors, int centroids_num, int centroid_size, int vectors_num, int max_iterations) {
+  double max_distance = eps + 1; 
+  int iter_couter = 0;
+  while (max_distance >= eps && iter_couter < max_iterations) {
+    max_distance = 0;
+    double **centroids_sum = (double**)calloc(centroids_num, sizeof(double *));
+    double *counters = (double*)calloc(centroids_num, sizeof(double));
+
+    for (int j=0; j<centroids_num; j++) {
+      centroids_sum[j] = calloc(centroid_size, sizeof(double));
+    }
+
+    for (int i=0; i<vectors_num; i++) {
+      int closest_centroid_index = find_closest_centroid_to_vector_index(vectors[i], centroid_size, centroids, centroids_num);
+      counters[closest_centroid_index]++;
+
+      for (int p=0; p<centroid_size; p++) {
+        centroids_sum[closest_centroid_index][p]+=vectors[i][p];
+      } 
+    }
+
+    for (int j=0; j<centroids_num; j++) {
+      if (counters[j] > 0) {
+        double* new_centroid_j = create_new_centroid(centroids_sum[j], counters[j], centroid_size);
+        double centroids_distance = calculate_distance(centroids[j], new_centroid_j, centroid_size);
+        if (centroids_distance > max_distance) {
+          max_distance = centroids_distance;
+        }
+        free(centroids[j]);
+        centroids[j]= new_centroid_j;
+      }
+    }
+
+    free(centroids_sum);
+    free(counters);
+
+    iter_couter++;
+  }
+}
+
+void print_output(double** centroids, int centroids_num, int centroid_size) {
+  for (int i=0; i<centroids_num; i++) {
+    for (int j=0; j<centroid_size; j++) {
+      printf("%.4f",centroids[i][j]);
+      if (j != centroid_size-1)
+        printf(",");
+      else
+        printf("\n");
+    }
+  }
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 4 && argc != 5) {
@@ -38,10 +150,7 @@ int main(int argc, char* argv[]) {
     int N = atoi(argv[2]);
     int d = atoi(argv[3]);
     int iter = argc == 5 ? atoi(argv[4]) : 200;
-    // printf("%d", K);
-    // printf("%d", N);
-    // printf("%d", d);
-    // printf("%d", iter);
+
   	if (iter <= 1 || iter >= 1000) {
       printf("Invalid maximum iteration!");
     }
@@ -51,138 +160,19 @@ int main(int argc, char* argv[]) {
     }
 
     FILE *fp = stdin;
-    char line[MAX_LINE_LENGTH];
-
-    double **vectors = (double**)calloc(N, sizeof(double *));
-    if (vectors == NULL) {
-            fprintf(stderr, "Memory allocation failed\n");
-            exit(1);
-    }
-    
-    double **centroids = calloc((size_t)K, sizeof(double *));
-    if (centroids == NULL) {
-            fprintf(stderr, "Memory allocation failed\n");
-            exit(1);
-    }
-
-    // int capacity = 0;
-    int line_count = 0;
-
     if (fp == NULL) {
       perror("Error opening input");
       exit(1);
     }
 
-    while (fgets(line, sizeof(line), fp) != NULL) {
-        line[strcspn(line, "\n")] = '\0';
+    double** vectors = input_file_to_matrix(fp, N, d);
+    double** centroids = deep_copy_matrix(vectors, K, d);
 
-        // if (line_count == capacity) {
-        //     capacity += 50;
-        //     vectors = realloc(vectors, capacity * sizeof(double *));
-        //     if (vectors == NULL) {
-        //         fprintf(stderr, "Memory allocation failed\n");
-        //         exit(1);
-        //     }
-        // }
-        
-        vectors[line_count] = calloc(d, sizeof(double));
+    calculate_centroids_convergence(centroids, vectors, K, d, N, iter);
 
-        if (vectors[line_count] == NULL) {
-            fprintf(stderr, "Memory allocation failed\n");
-            exit(1);
-        }
+    print_output(centroids, K, d);
 
-        // Tokenize the input string using strtok
-        char *token = strtok(line, ",");
-
-        int tokensCounter = 0;        
-        while (token != NULL) {
-            vectors[line_count][tokensCounter] = strtof(token, NULL);
-            
-            token = strtok(NULL, ",");
-            tokensCounter++;
-        }
-        line_count++;
-    }
-
-    
-    // Deep copy centroids
-    for (int i=0; i<K; i++) {
-      centroids[i] = calloc(d, sizeof(double));
-      
-      for (int j=0; j<d; j++) {
-        centroids[i][j] = vectors[i][j];
-      }
-    }
-
-
-
-    double max_distance = eps + 1; 
-    int iter_couter = 0;
-    while (max_distance >= eps && iter_couter < iter) {
-      max_distance = 0;
-      double **centroids_sum = (double**)calloc(K, sizeof(double *));
-      double *counters = (double*)calloc(K, sizeof(double));
-
-      for (int j=0; j<K; j++) {
-        centroids_sum[j] = calloc(d, sizeof(double));
-      }
-
-      for (int i=0; i<N; i++) {
-        double min_distance = distance(vectors[i], centroids[0], d);
-        int min_j = 0;
-        for (int j=0; j<K; j++) {
-          double distance_from_centroid = distance(vectors[i], centroids[j], d);
-          if (min_distance > distance_from_centroid) {
-            min_distance = distance_from_centroid;
-            min_j = j;
-          }
-        }
-        counters[min_j]++;
-        // centroids_sum[j]+=vectors[i];
-        for (int p=0; p<d; p++) {
-          centroids_sum[min_j][p]+=vectors[i][p];
-        } 
-      }
-
-      for (int j=0; j<K; j++) {
-        if (counters[j] > 0) {
-          double* new_centroid_j = calloc(d, sizeof(double));
-          for (int p=0; p<d; p++) {
-            new_centroid_j[p] = centroids_sum[j][p] / counters[j];
-          }
-          double centroids_distance = distance(centroids[j], new_centroid_j, d);
-          // printf("centroids distance in itertaion %d is: %f \n", j, counters[j]);
-          if (centroids_distance > max_distance) {
-            max_distance = centroids_distance;
-          }
-          // free(centroids[j]);
-          for (int p=0; p<d; p++) {
-            centroids[j][p] = new_centroid_j[p];
-          }
-          free(new_centroid_j);
-        }
-        // centroids[j] = new_centroid_j;
-      }
-
-      // Free memory
-      free(centroids_sum);
-      free(counters);
-
-      iter_couter++;
-    }
-
-    for (int i=0; i<K; i++) {
-        for (int j=0; j<d; j++) {
-          printf("%.4f",centroids[i][j]);
-          if (j != d-1)
-            printf(",");
-          else
-            printf("\n");
-        }
-    }
-
-    for (int i = 0; i < line_count; i++) {
+    for (int i = 0; i < N; i++) {
         free(vectors[i]);
     }
     free(vectors);
@@ -190,8 +180,8 @@ int main(int argc, char* argv[]) {
       free(centroids[i]);
     }
     free(centroids);
-    // free(line);
     fclose(fp);
+
     return 0;
 }
 
