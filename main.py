d539c0474e0b12c57d1372591c98798b7e025045
GuyKoch <guykoch@mail.tau.ac.il>
2024-02-03 17:16:16 +0200

    Spliting python, refreshing the test_readme according to web update and running on nova

--- main.py
+++ main.py
@@ -2,39 +2,46 @@ from math import sqrt
 import sys
 eps = 0.0001
 
-def distance1(vector1, vector2, d):
+
+def validate_params():
+    if not (len(sys.argv) != 5 and len(sys.argv) != 6):
+        K = int(sys.argv[1])
+        N = int(sys.argv[2])
+        d = int(sys.argv[3])
+        iter = 200 if len(sys.argv) == 5 else int(sys.argv[4])
+        if(iter <= 1 or iter >= 1000):
+            print("Invalid maximum iteration!")
+        if (K <= 1 or K >=N):
+            print("Invalid number of clusters!")
+        filePath = sys.argv[5] if len(sys.argv) == 6 else sys.argv[4]
+        return(K, N, d, iter, filePath)
+    else:
+        return 1
+
+def distance(vector1, vector2, d):
     sum = 0
     for i in range(d):
         sum+=(vector1[i]-vector2[i])**2
     return sqrt(sum)
 
-
-def main():
-    #if(len(sys.argv) != 5 and len(sys.argv) != 6):
-        #return 1    
-    K = 3 # K = int(sys.argv[1])
-    N = 800 # int(sys.argv[2])
-    d = 3 # int(sys.argv[3])
-    iter = 600 # 200 if len(sys.argv) == 5 else int(sys.argv[4])
-    if(iter <= 1 or iter >= 1000):
-        print("Invalid maximum iteration!")
-    if (K <= 1 or K >=N):
-        print("Invalid number of clusters!")
-
-    
+def initialize_vectors_array(filePath):
     vectors = []
-    centroids = []
-
-    filePath = "tests\input_1.txt" # sys.argv[5] if len(sys.argv) == 6 else sys.argv[4]
     with open(filePath, 'r') as fp:
         for line in fp:
             line = line.split(',')
             line = [float(item) for item in line]
             vectors.append(line)
+    return vectors
 
+def initialize_centroids_array(vectors):
+    centroids = []
     for i in range(K):
         centroids.append(vectors[i].copy())
+    return centroids
 
+def main(K, N, d, iter, filePath): 
+    vectors = initialize_vectors_array(filePath=filePath)  
+    centroids = initialize_centroids_array(vectors)
     max_distance = eps + 1
     iter_counter = 0
 
@@ -44,10 +51,10 @@ def main():
         centroids_sum = [[0 for _ in range(d)] for _ in range(K)]
         counters = [0 for _ in range(K)]
         for i in range(N):
-            min_distance = distance1(vectors[i], centroids[0], d)
+            min_distance = distance(vectors[i], centroids[0], d)
             min_j = 0
             for j in range(K):
-                distance_from_centroid = distance1(vectors[i], centroids[j], d)
+                distance_from_centroid = distance(vectors[i], centroids[j], d)
                 if min_distance > distance_from_centroid:
                     min_distance = distance_from_centroid
                     min_j = j
@@ -57,7 +64,7 @@ def main():
         for j in range(K):
             if counters[j] != 0:
                 new_centroid_j = [centroids_sum[j][p] / counters[j] for p in range(d)]
-                centroids_distance = distance1(centroids[j], new_centroid_j, d)
+                centroids_distance = distance(centroids[j], new_centroid_j, d)
                 max_distance = max(max_distance, centroids_distance)
                 centroids[j] = new_centroid_j.copy()
         iter_counter += 1
@@ -65,15 +72,6 @@ def main():
     for item in centroids:
         print(','.join([str(round(float(num), 4)) for num in item]))
 
-            
-
-
-
-            
-
-            
-main()
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    K, N, d, iter, filePath = validate_params()
+    main(K, N, d, iter, filePath)
